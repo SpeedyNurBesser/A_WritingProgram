19ce803ce27c25e875c37f83aeebbd2a86832027
Ole370 <70910518+SpeedyNurBesser@users.noreply.github.com>
2024-07-17 17:10:35 +0200

    Updated main.py
    added markdown marking shortcuts (e.g. ctrl + i -> italic) to BetterText widget
    Most groundwork already existed in last commit, finally implemented a unmarking method for selected text

--- main.py
+++ main.py
@@ -22,15 +22,16 @@ class BetterText(tk.Text):
         # adding to changes list, when the text is altered, i.e. a button is pressed 
         self.bind('<Key>', self.add_changes)
 
+
         # Markdown shortcuts
         self.bind('<Control-i>', self.italicText) # italizes text 
-        self.bind('<Control-b>', self.boldText) # bold(s)? text
+        self.bind('<Control-b>', self.boldText) # bold(s)? text -> prints text in bold
         self.bind('<Control-u>', self.underlinedText) # underlines text
-        self.bind('<Control-h>', self.highlightText) # highlight text
 
-#TODO: add markdown shortcut stuff (Ctrl+I -> *word*)
+
 #TODO: add Ctrl + Backspace entire word removal.
 
+    # Undo/Redo
     def undo(self, event=None):
         if self.steps != 0:
             self.steps -= 1
@@ -48,8 +49,10 @@ class BetterText(tk.Text):
             self.changes.append(self.get('1.0','end'))
             self.steps += 1
 
+
+    # markdown shortcuts
     def postItalicText(self, event=None):
-        # delete last typed character (tab)
+        # delete last typed character (tab) (for explanation see italicText method)
         cursorPos = self.index(tk.INSERT)
         self.delete(f'{cursorPos} - 1 chars', cursorPos)
 
@@ -57,7 +60,7 @@ class BetterText(tk.Text):
         # italize text
         self.markText(SYNTAX_FRONT='*', SYNTAX_BACK='*')
         self.after(1, self.postItalicText)
-        # Ctrl+I is interpreted as Tab; to circumvent this a function is called 1 ms after the text has been marked that instantly deletes the tab again; this only works, because the Tab is always send after the function call
+        # Ctrl+I is interpreted as Tab; to circumvent this a function is called 1 ms after the text has been marked that instantly deletes the tab again; this only works, because the Tab is always only send after the function (-> method) call
         
     def boldText(self, event=None): # **bold**
         self.markText(SYNTAX_FRONT='**', SYNTAX_BACK='**')
@@ -65,23 +68,59 @@ class BetterText(tk.Text):
     def underlinedText(self, event=None): # <u>underline</u>
         self.markText(SYNTAX_FRONT='<u>', SYNTAX_BACK='</u>')
 
-    def highlightText(self, event=None): # ==highlight==
-        self.markText(SYNTAX_FRONT='==', SYNTAX_BACK='==')
+    def isMarked(self, SYNTAX_FRONT, SYNTAX_BACK):
+        selectionRange = self.tag_ranges('sel')
+
+        foregoingTextPos = (f'{selectionRange[0]} - {len(SYNTAX_FRONT)} chars', selectionRange[0])
+        foregoingText = self.get(foregoingTextPos[0], foregoingTextPos[1])
+
+        firstCharPos = (selectionRange[0], f'{selectionRange[0]} + {len(SYNTAX_FRONT)} chars')
+        firstChars = self.get(firstCharPos[0], firstCharPos[1])
+
+        followingTextPos = (selectionRange[1], f'{selectionRange[1]} + {len(SYNTAX_BACK)} chars')
+        followingText = self.get(followingTextPos[0], followingTextPos[1])
+
+        lastCharPos = (f'{selectionRange[1]} - {len(SYNTAX_BACK)} chars', selectionRange[1])
+        lastChars = self.get(lastCharPos[0], lastCharPos[1])
+
+        if SYNTAX_FRONT == foregoingText and SYNTAX_BACK == followingText:
+            return (True, foregoingTextPos, followingTextPos)
+        
+        elif SYNTAX_FRONT == firstChars and SYNTAX_BACK == lastChars:
+            return (True, firstCharPos, lastCharPos)
+
+        else:
+            return (False, None)
 
     def markText(self, SYNTAX_FRONT='*', SYNTAX_BACK='*'):
-        if self.tag_ranges('sel'):
-            selection = self.selection_get()
-            selection = f'{SYNTAX_FRONT}{selection}{SYNTAX_BACK}'
-            self.insert(self.tag_ranges('sel')[1], selection)
-            self.delete(self.tag_ranges('sel')[0], self.tag_ranges('sel')[1])
+        # marks text using a given syntax (markdown syntax (*italic*))
+        # if any text is selected, toggle marks on selected text
+        # else insert SYNTAX at textCursor
+
+        if self.tag_ranges('sel'): # if any text is selected
+            isMarked = self.isMarked(SYNTAX_FRONT, SYNTAX_BACK)
+            selection = self.selection_get() # store the selected text
+
+            if isMarked[0] == True: # check if text is already marked
+                # syntax found -> delete syntax
+                # note: deletes back-syntax before front-syntax; for otherwise the positions would change
+                self.delete(isMarked[2][0], isMarked[2][1])
+                self.delete(isMarked[1][0], isMarked[1][1])
+            else: # no syntax found -> add syntax
+                selection = f'{SYNTAX_FRONT}{selection}{SYNTAX_BACK}' # add Syntax to stored selection
+                self.insert(self.tag_ranges('sel')[1], selection)  # insert new stored selection right after the real selection
+                self.delete(self.tag_ranges('sel')[0], self.tag_ranges('sel')[1]) # delete original selection
+            
+            # unselects everything
             self.tag_remove('sel', '1.0', 'end')
+
         else:
             cursorPos = self.index(tk.INSERT)
             self.insert(cursorPos, f'{SYNTAX_FRONT}{SYNTAX_BACK}')
             self.mark_set('insert', self.calcCursorMove(move=-(len(SYNTAX_BACK)), position=self.index(tk.INSERT))) # applies new position (inside Syntax) to cursor
 
 
-    def calcCursorMove(self, move=-1, position='1.0'): # calculates the new position the text cursor should have when moved by a given value to the left (negative int) or the right (positive int); starting from a given position
+    def calcCursorMove(self, move=-1, position='1.0'): # calculates the new position the text cursor should have when moved by a given move-value to the left (negative int) or the right (positive int); starting from a given position
         cursorPos = position.split('.') # splits the given cursor position into a line string [0] and a char string [1]
         cursorPos[1] = str(int(cursorPos[1]) + move) # modifies the char position
         cursorPos = '' + str(cursorPos[0]) + '.' + str(cursorPos[1]) # applies the needed position format: 'line.char' to cursorPos variable
@@ -185,7 +224,6 @@ class Writer(): # a tkinter window for distraction-free writing
 
 #TODO: stylize the progress bar; it honestly doesn't really look good rn
 #TODO: deactivate other monitors
-#TODO: undo/redo
 
 class WriterStartup():
     def __init__(self) -> None:
@@ -268,6 +306,9 @@ if __name__ == '__main__':
     #Writer(blockStyle=0).run()
     #WriterStartup()
     root = tk.Tk()
+
+    root.geometry('500x300')
+    
 
     text = BetterText(root, wrap='word', font=('Times New Roman', 16), width=90)
     text.pack()
